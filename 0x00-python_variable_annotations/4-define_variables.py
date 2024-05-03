34b41479e945b2d6a5fcf3476b358a3f685d5bf9
tessykc <tessykc@gmail.com>
2024-05-03 09:31:34 +0100

    Comment

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+a: int = 1  # an integer with a value of 1
+pi: float = 3.14  # a float with a value of 3.14
+i_understand_annotations: bool = True  # a boolean with a value of True
+school: str = "Holberton"  # a string with a value of “Holberton”

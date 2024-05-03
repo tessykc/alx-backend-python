77fdfd001c79b748330183e8721e6b345942c527
tessykc <tessykc@gmail.com>
2024-05-03 09:43:14 +0100

    Update

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
 
-from typing import TypeVar, Mapping, Any, Union
-
-# Define a type variable for the default value
-T = TypeVar("T")
+from typing import Sequence, Any, Union
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """
-    Safely gets a value from a dictionary.
+    Safely returns the first element of a list.
 
     Args:
-        dct (Mapping): The input dictionary.
-        key (Any): The key to look up in the dictionary.
-        default (Union[T, None], optional): The default value to return
-        if the key is not found. Defaults to None.
+        lst (Sequence[Any]): The input list.
 
     Returns:
-        Union[Any, T]: The value associated with the key, or the
-        default value if the key is not found.
+        Union[Any, None]: The first element of the list if it exists,
+        otherwise None.
     """
-    if key in dct:
-        return dct[key]
+    if lst:
+        return lst[0]
     else:
-        return default
+        return None

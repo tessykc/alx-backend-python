30bd9373a2174267bf98da9a2f90d47a4b9aa645
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-08 11:39:53 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,5 +3,6 @@
 import asyncio
 from 0-async_generator import async_generator
 
+
 async def async_comprehension():
     return [i async for i in async_generator()]: [i async for i in async_generator()]

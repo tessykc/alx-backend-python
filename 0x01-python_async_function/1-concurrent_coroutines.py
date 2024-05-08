0e7f770b79232bd3f7d918e1cbe054e5421df6b2
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-08 11:28:07 +0100

    Update 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python3
+!/usr/bin/env python3
 
 import asyncio
 from typing import List
 
+# Import wait_random from the previous file
 from 0x01_python_async_function import wait_random
 
 
@@ -11,18 +12,18 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Spawn wait_random n times with the specified max_delay.
 
     Args:
-        n (int): Number of times to spawn wait_random.
-        max_delay (int): Maximum delay for wait_random.
+        n (int): The number of times to spawn wait_random.
+        max_delay (int): The maximum delay for wait_random.
 
     Returns:
-        List[float]: Sorted list of delay times.
+        List[float]: A list of the delays in ascending order.
     """
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    completed, _ = await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
-    return sorted([task.result() for task in completed])
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
 
 
 if __name__ == "__main__":
-    print(asyncio.run(wait_n(5, 5)))
-    print(asyncio.run(wait_n(10, 7)))
-    print(asyncio.run(wait_n(10, 0)))
+    asyncio.run(wait_n(5, 5))
+    asyncio.run(wait_n(10, 7))
+    asyncio.run(wait_n(10, 0))
+    

2e3996ba1dc1912e3d24747a5f3a0fb3c52c1034
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-08 08:59:08 +0100

    Create 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,15 @@
+import asyncio
+import time
+from 1-async_comprehension import async_comprehension
+
+
+async def measure_runtime():
+    start_time = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end_time = time.time()
+    return end_time - start_time

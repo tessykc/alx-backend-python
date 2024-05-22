ca378f790b4f1972aff4d4178f01cca8a56fb95b
Kelechi Obi <72105538+tessykc@users.noreply.github.com>
2024-05-22 17:53:06 +0100

    Update 4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+"""Task wait n"""
 
 import asyncio
 from asyncio import tasks4-tasks.py
@@ -8,7 +8,9 @@ from 3-tasks import task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> list:
-    """Call task_wait_random n times with the specified max_delay."""
+    """
+    Call task_wait_random n times with the specified max_delay.
+    """
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
 
